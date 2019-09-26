d3a8721beca0d75ae8644a16fc862c4f368e1bd0
Arithmetics <brock.m.tillotson@gmail.com>
2019-09-25 20:55:30 -0700

    added some react hooks and got basic api call for owners working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [owners, setOwners] = useState(["matt", "brock", "kevin"]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("http://localhost:3001/owners");
+      console.log(result);
+      setOwners(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {owners.map((owner, index) => (
+        <Owner key={index} owner={owner} />
+      ))}
     </div>
   );
 }
 
 export default App;
+
+function Owner({ owner }) {
+  return (
+    <div>
+      <h3>
+        {owner.id} - {owner.name}
+      </h3>
+      <ul>
+        {owner.fantasy_teams &&
+          owner.fantasy_teams.length > 0 &&
+          owner.fantasy_teams.map(team => (
+            <li key={team.id}>
+              {team.id} - {team.name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
