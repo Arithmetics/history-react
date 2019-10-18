c28e6210672bc87f9d91f3390222eb6c347b01ae
Arithmetics <brock.m.tillotson@gmail.com>
2019-10-17 20:59:23 -0700

    hardcoded

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 const prod = "http://75.164.135.106:8080";
 const dev = "http://localhost:8080";
 
-export const config = process.env.NODE_ENV === "development" ? dev : prod;
+// export const config = process.env.NODE_ENV === "development" ? dev : prod;
+
+export const config = "http://75.164.135.106:8080";
