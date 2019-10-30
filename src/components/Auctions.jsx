9a1f9fdf63b9be99bcb2f40cdd8c980333fd3fd7
Brock Tillotson <brocktillotson@6degreeshealth.com>
2019-10-30 13:46:23 -0700

    messing around with some possible table controls

--- src/components/Auctions.jsx
+++ src/components/Auctions.jsx
@@ -2,22 +2,101 @@ import React, { useState, useEffect } from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import axios from "axios";
 import Container from "react-bootstrap/Container";
-import Table from "react-bootstrap/Table";
 import { config } from "../api";
-import { Link } from "react-router-dom";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory, {
+  textFilter,
+  numberFilter,
+  Comparator
+} from "react-bootstrap-table2-filter";
 
 function Auctions() {
   const [auction, setAuction] = useState([]);
 
+  const sampleData = [
+    {
+      id: 144,
+      price: 67,
+      position: "QB",
+      year: 2018,
+      player: { id: 2506363, name: "Aaron Rodgers" },
+      fantasyTeam: { id: 56, name: "Max 30 characters No profanity" },
+      owner: { id: 8, name: "joe" }
+    },
+    {
+      id: 282,
+      price: 61,
+      position: "RB",
+      year: 2018,
+      player: { id: 2552475, name: "Todd Gurley" },
+      fantasyTeam: { id: 8, name: "MAN FUQ KJEMP" },
+      owner: { id: 1, name: "Dag" }
+    },
+    {
+      id: 655,
+      price: 56,
+      position: "QB",
+      year: 2018,
+      player: { id: 2532975, name: "Russell Wilson" },
+      fantasyTeam: { id: 24, name: "JEFFERY" },
+      owner: { id: 3, name: "Brock" }
+    },
+    {
+      id: 722,
+      price: 56,
+      position: "QB",
+      year: 2018,
+      player: { id: 2504211, name: "Tom Brady" },
+      fantasyTeam: { id: 83, name: "Dakstreet's Back" },
+      owner: { id: 12, name: "Brandon" }
+    },
+    {
+      id: 559,
+      price: 53,
+      position: "QB",
+      year: 2018,
+      player: { id: 2504775, name: "Drew Brees" },
+      fantasyTeam: { id: 96, name: "Bottom Feeder Baby" },
+      owner: { id: 16, name: "Dennis" }
+    },
+    {
+      id: 654,
+      price: 52,
+      position: "QB",
+      year: 2018,
+      player: { id: 2495455, name: "Cam Newton" },
+      fantasyTeam: { id: 24, name: "JEFFERY" },
+      owner: { id: 3, name: "Brock" }
+    },
+    {
+      id: 720,
+      price: 52,
+      position: "RB",
+      year: 2018,
+      player: { id: 2552461, name: "Duke Johnson" },
+      fantasyTeam: { id: 83, name: "Dakstreet's Back" },
+      owner: { id: 12, name: "Brandon" }
+    },
+    {
+      id: 343,
+      price: 51,
+      position: "RB",
+      year: 2018,
+      player: { id: 2540175, name: "Le'Veon Bell" },
+      fantasyTeam: { id: 16, name: "Fournetteflix and Chill" },
+      owner: { id: 2, name: "Kevin" }
+    }
+  ];
+
   useEffect(() => {
-    console.log("asdfasdf");
     const fetchData = async () => {
       console.log(`${config}/purchases.json`);
       const result = await axios(`${config}/purchases.json`);
       console.log(result);
       setAuction((result && result.data && result.data.purchases) || []);
     };
-    fetchData();
+    setAuction(sampleData);
+    // fetchData();
   }, []);
 
   return (
@@ -34,44 +113,56 @@ function Auctions() {
 }
 
 export function AuctionTable({ auction }) {
+  const columns = [
+    {
+      dataField: "year",
+      text: "Year",
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      dataField: "player.name",
+      text: "Player",
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      dataField: "position",
+      text: "Position",
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      dataField: "owner.name",
+      text: "Owner",
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      dataField: "fantasyTeam.name",
+      text: "Team",
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      dataField: "price",
+      text: "Price",
+      sort: true,
+      filter: numberFilter({
+        defaultValue: { number: 0, comparator: Comparator.GT }
+      })
+    }
+  ];
   return (
     <>
       <h3>Auctions</h3>
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Player</th>
-            <th>Position</th>
-            <th>Owner</th>
-            <th>Team</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {auction.map((purchase, i) => (
-            <tr key={i}>
-              <td>{purchase.year}</td>
-              <td>
-                <Link to={`/players/${purchase.player && purchase.player.id}`}>
-                  {purchase.player && purchase.player.name}
-                </Link>
-              </td>
-              <td>{purchase.position}</td>
-              <td>{purchase.owner && purchase.owner.name}</td>
-              <td>
-                <Link
-                  to={`/fantasyTeams/${purchase.fantasyTeam &&
-                    purchase.fantasyTeam.id}`}
-                >
-                  {purchase.fantasyTeam && purchase.fantasyTeam.name}
-                </Link>
-              </td>
-              <td>{purchase.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <BootstrapTable
+        bootstrap4
+        keyField="id"
+        data={auction}
+        columns={columns}
+        filter={filterFactory()}
+      />
     </>
   );
 }
