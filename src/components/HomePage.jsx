3264f035a12cd871ad3045e40c41e5553145f6bb
Arithmetics <brock.m.tillotson@gmail.com>
2020-06-15 13:41:24 -0700

    basic table

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,19 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import clsx from "clsx";
+
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
+import MaterialTable from "material-table";
+
+import LoadingSpinner from "./LoadingSpinner";
+import { config } from "../api";
+
 export default function HomePage() {
+  const [versusRecords, setVersusRecords] = useState([]);
+  const [scheduledGames, setScheduledGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(`${config}/home/show.json`);
+      setVersusRecords(
+        (result && result.data && result.data.versusRecords) || []
+      );
+      setScheduledGames(
+        (result && result.data && result.data.scheduledGames) || []
+      );
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const currentWeek =
+    scheduledGames && scheduledGames.length > 0 && scheduledGames[0].week;
+
   return (
     <>
-      <Typography>Welcome to the lab....</Typography>
-      <iframe
-        title="old lotto"
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/TVTJwGAjkog"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <Typography variant="h3">Welcome to the lab....</Typography>
+      {loading && <LoadingSpinner isLoading={loading} />}
+      {!loading && (
+        <>
+          <MaterialTable
+            data={scheduledGames}
+            options={{
+              paging: false,
+              sorting: false,
+              search: false,
+              showTitle: true,
+              exportButton: false,
+            }}
+            title={`Week ${currentWeek} Upcoming Games`}
+            columns={[
+              {
+                title: "Away Owner",
+                field: "awayTeam.owner.name",
+              },
+              {
+                title: "Away Team",
+                field: "awayTeam.name",
+              },
+              {
+                title: "Home Team",
+                field: "homeTeam.name",
+              },
+              {
+                title: "Home Owner",
+                field: "homeTeam.owner.name",
+              },
+            ]}
+          />
+        </>
+      )}
     </>
   );
 }
