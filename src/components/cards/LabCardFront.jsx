d5a5b633c55a4c000667744819e7180b368ef142
Arithmetics <brock.m.tillotson@gmail.com>
2021-02-21 11:57:53 -0800

    lab front card component made

--- src/components/cards/LabCardFront.jsx
+++ src/components/cards/LabCardFront.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import clsx from 'clsx';
+import { GiTrophy } from 'react-icons/gi';
+import { BsFillLightningFill } from 'react-icons/bs';
 
 import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import FlipCameraAndroidIcon from '@material-ui/icons/FlipCameraAndroid';
-
-import LabCardFront from './LabCardFront';
-import LabCardBack from './LabCardBack';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -87,6 +84,95 @@ const useStyles = makeStyles((theme) => ({
     transform: 'rotateY(-180deg)',
     zIndex: 30,
   },
+  playerPicture: {
+    height: 400,
+    width: 250,
+    backgroundPosition: 'center',
+    margin: '23px 47px',
+    position: 'relative',
+    backfaceVisibility: 'hidden',
+  },
+  coverShape: {
+    position: 'absolute',
+    backgroundColor: '#191818',
+    clipPath:
+      'polygon(0 0, 93% 0, 100% 21%, 100% 100%, 7% 100%, 0 76%)',
+    width: 300,
+    height: 82,
+    bottom: -43,
+    left: -29,
+  },
+  leftCoverShape: {
+    position: 'absolute',
+    backgroundColor: '#191818',
+    clipPath: 'polygon(52% 0, 100% 0, 100% 100%, 0 100%, 0 5%)',
+    left: -29,
+    width: 35,
+    bottom: 38,
+    height: 362,
+  },
+  leftCoverInner: {
+    width: '100%',
+    height: '100%',
+    position: 'relative',
+  },
+  firstName: {
+    color: 'white',
+    textAlign: 'left',
+    fontSize: 24,
+    fontVariant: 'petite-caps',
+    margin: 0,
+    marginLeft: 32,
+    zIndex: 10,
+  },
+  lastName: {
+    color: 'white',
+    textAlign: 'left',
+    fontSize: 36,
+    fontVariant: 'petite-caps',
+    margin: 0,
+    marginLeft: 44,
+    marginTop: -10,
+    zIndex: 10,
+  },
+  year: {
+    color: '#fff',
+    transform: 'rotate(-90deg)',
+    fontSize: 20,
+    position: 'absolute',
+    top: 33,
+    left: -3,
+  },
+  avatars: {
+    position: 'absolute',
+    zIndex: 2,
+    right: 11,
+    top: 9,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backfaceVisibility: 'hidden',
+  },
+  avatarPic: {
+    width: 50,
+    height: 50,
+    borderRadius: '50%',
+    border: '2px solid black',
+    borderColor: theme.palette.primary.main,
+    backfaceVisibility: 'hidden',
+  },
+  smallAvatar: {
+    width: 30,
+    height: 30,
+    marginTop: 5,
+    borderRadius: '50%',
+    border: '2px solid black',
+    borderColor: theme.palette.primary.main,
+    backgroundColor: 'black',
+    color: theme.palette.primary.main,
+    marginRight: 2,
+    paddingTop: 2,
+  },
   flipButton: {
     width: 50,
     height: 50,
@@ -148,37 +234,43 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function LabCard() {
+function LabCardFront() {
   const classes = useStyles();
 
-  const [flipped, setFlipped] = useState(false);
-
-  const flipCard = () => setFlipped(!flipped);
-
   return (
-    <div className={classes.card}>
-      <IconButton
-        aria-label="flip"
-        className={classes.flipButton}
-        onClick={flipCard}
-      >
-        <FlipCameraAndroidIcon />
-      </IconButton>
-      <div
-        className={clsx(
-          classes.cardInner,
-          flipped ? classes.cardInnerFlipped : undefined,
-        )}
-      >
-        <div className={clsx(classes.cardSide, classes.cardFront)}>
-          <LabCardFront />
+    <>
+      <div className={classes.avatars}>
+        <img
+          src="/ownerAvatars/50_x_50/13.png"
+          className={classes.avatarPic}
+          alt="owner-img"
+        />
+
+        <div className={classes.smallAvatar}>
+          <GiTrophy />
         </div>
-        <div className={clsx(classes.cardSide, classes.cardBack)}>
-          <LabCardBack />
+        <div className={classes.smallAvatar}>R</div>
+        <div className={classes.smallAvatar}>
+          <BsFillLightningFill />
         </div>
       </div>
-    </div>
+
+      <div
+        className={classes.playerPicture}
+        style={{ backgroundImage: 'url(/cards/2555224.jpg)' }}
+      >
+        <div className={classes.coverShape}>
+          <p className={classes.firstName}>Ezekiel</p>
+          <p className={classes.lastName}>Elliot</p>
+        </div>
+        <div className={classes.leftCoverShape}>
+          <div className={classes.leftCoverInner}>
+            <p className={classes.year}>2019</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
-export default LabCard;
+export default LabCardFront;
